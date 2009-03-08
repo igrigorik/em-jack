c76d38a4d726084245d139bdbb4997ee06355fe3
dan sinclair <dj2@everburning.com>
2009-03-08 01:03:26 -0500

    match current reality

--- example.rb
+++ example.rb
@@ -1,44 +1,24 @@
-require 'jack'
 
 jack = Jack::Connection.new
 
-# default puts errback will be assigned...
+r = jack.use('mytube')
+r.callback do |tube|
+  puts "Using #{tube}"
+end
 
 r = jack.reserve
 r.callback do |job|
   puts job.jobid
   process(job)
 end
-r.errback do |reason|
-  puts "Failed to reserve job #{reason}"
-end
-
-jack.reserve do |job|
-  puts job.jobid
-  process(job)
-end
 
 r = jack.delete(job)
-r.callback do |jobid|
-  puts "Successfully delete #{jobid}"
-end
-r.errback do |jobid, reason|
-  puts "Failed to delete #{jobid} :: #{reason}"
-end
-
-r = jack.stats(:tube)
-r.callback do |stats|
-  puts stats.inspect
-end
-r.errback do |error|
-  puts "Failed to get stats:: #{error}"
+r.callback do
+  puts "Successfully deleted"
 end
 
 r = jack.put("my message", :ttr => 300)
 r.callback do |jobid|
   puts "put successful #{jobid}"
-end
-r.errback do |msg, opts, reason|
-  puts "Failed ot put #{msg}:: #{reason}"
 end
 
