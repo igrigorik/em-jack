d2579ba2b01b61f90d7534bc664704d1eff60eed
dan sinclair <dj2@everburning.com>
2010-03-24 22:59:12 -0400

    add touched handler

--- lib/em-jack/handlers/touched.rb
+++ lib/em-jack/handlers/touched.rb
@@ -7,7 +7,7 @@ module EMJack
         response =~ RESPONSE
       end
 
-      def self.handle(deferrable, response, body)
+      def self.handle(deferrable, response, body, conn=nil)
         return false unless response =~ RESPONSE
 
         deferrable.succeed
