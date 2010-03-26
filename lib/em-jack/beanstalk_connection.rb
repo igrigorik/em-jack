9fc6609ae9fc625f36718d3e2382ac4d5cbc30be
dj2 <dj2@everburning.com>
2010-03-26 17:42:29 -0400

    whitespace

--- lib/em-jack/beanstalk_connection.rb
+++ lib/em-jack/beanstalk_connection.rb
@@ -3,7 +3,7 @@ require 'eventmachine'
 module EMJack
   class BeanstalkConnection < EM::Connection
     attr_accessor :client
-  
+
     def connection_completed
       @client.connected
     end
@@ -11,18 +11,18 @@ module EMJack
     def receive_data(data)
       @client.received(data)
     end
-  
+
     def send(command, *args)
       cmd = command.to_s
       cmd << " #{args.join(" ")}" unless args.length == 0
       cmd << "\r\n"
       send_data(cmd)
     end
-  
+
     def send_with_data(command, data, *args)
       send_data("#{command.to_s} #{args.join(" ")}\r\n#{data}\r\n")
     end
-  
+
     def unbind
       @client.disconnected
     end
