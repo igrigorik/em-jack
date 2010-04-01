1955c9312cfc2c72eebcbcca5d0ed9ff9d60e543
dj2 <dj2@everburning.com>
2010-04-01 11:40:20 -0400

    use let in job spec

--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -5,26 +5,24 @@ require 'spec'
 require 'em-jack'
 
 describe EMJack::Job do
-  before(:each) do
-    @conn = mock(:conn)
-  end
+  let (:conn ) { mock(:conn) }
 
   it 'converts jobid to an integer' do
     j = EMJack::Job.new(nil, "1", "body")
     j.jobid.class.should == Fixnum
     j.jobid.should == 1
   end
-  
+
   it 'sends a delete command to the connection' do
-    j = EMJack::Job.new(@conn, 1, "body")
-    @conn.should_receive(:delete).with(j)
-    
+    j = EMJack::Job.new(conn, 1, "body")
+    conn.should_receive(:delete).with(j)
+
     j.delete
   end
 
   it 'sends a stats command to the connection' do
-    j = EMJack::Job.new(@conn, 2, 'body')
-    @conn.should_receive(:stats).with(:job, j)
+    j = EMJack::Job.new(conn, 2, 'body')
+    conn.should_receive(:stats).with(:job, j)
 
     j.stats
   end
@@ -32,22 +30,22 @@ describe EMJack::Job do
   it 'sends a release command to the connection' do
     blk = Proc.new { x = 1 }
 
-    j = EMJack::Job.new(@conn, 2, 'body')
-    @conn.should_receive(:release).with(j, {:foo => :bar}, &blk)
+    j = EMJack::Job.new(conn, 2, 'body')
+    conn.should_receive(:release).with(j, {:foo => :bar}, &blk)
 
     j.release({:foo => :bar}, &blk)
   end
 
   it 'sends a touch command to the connection' do
-    j = EMJack::Job.new(@conn, 2, 'body')
-    @conn.should_receive(:touch).with(j)
+    j = EMJack::Job.new(conn, 2, 'body')
+    conn.should_receive(:touch).with(j)
 
     j.touch
   end
 
   it 'sends a bury command to the connection' do
-    j = EMJack::Job.new(@conn, 2, 'body')
-    @conn.should_receive(:bury).with(j, 1234)
+    j = EMJack::Job.new(conn, 2, 'body')
+    conn.should_receive(:bury).with(j, 1234)
 
     j.bury(1234)
   end
