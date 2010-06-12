44f8593b20d6357c5ce15d4384220ba372d5e25f
dan sinclair <dj2@everburning.com>
2010-06-12 00:25:13 -0400

    wrap in version check

--- spec/fiber_spec.rb
+++ spec/fiber_spec.rb
@@ -1,26 +1,27 @@
 require 'rubygems'
 require 'spec'
 
-require 'em-jack'
-require 'fiber'
+if RUBY_VERSION > '1.9'
+  require 'em-jack'
+  require 'fiber'
 
-describe EMJack::Connection do
-  it "should process live messages" do
-    EM.run do
-      EM.add_timer(10) { EM.stop }
-      
-      Fiber.new do
-        bean = EMJack::Connection.new
-        bean.fiber!
-        
-        bean.put("hello!")
-        job = bean.reserve
-        job.body.should == "hello!"
-        job.delete
-        
-        p bean.stats
-        EM.stop
-      end.resume
+  describe EMJack::Connection do
+    it "should process live messages" do
+      EM.run do
+        EM.add_timer(10) { EM.stop }
+
+        Fiber.new do
+          bean = EMJack::Connection.new
+          bean.fiber!
+
+          bean.put("hello!")
+          job = bean.reserve
+          job.body.should == "hello!"
+          job.delete
+
+          EM.stop
+        end.resume
+      end
     end
   end
 end
